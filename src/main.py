5b8e6985a6f7c1ef62658c525d71d699e6084b5d
Maciej Malinowski <maciej.malinowski7@gmail.com>
2024-06-26 12:28:52 +0200

    feat: include TodoCategories router and close session in lifespan on shutdown

--- src/main.py
+++ src/main.py
@@ -4,7 +4,9 @@ from typing import AsyncGenerator
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from src.api import api_router
 from src.config import app_configs, settings
+from src.db.session import sessionmanager
 
 
 @asynccontextmanager
@@ -12,6 +14,9 @@ async def lifespan(_application: FastAPI) -> AsyncGenerator:
     # Startup
     yield
     # Shutdown
+    if sessionmanager.engine is not None:
+        # Close the DB connection
+        await sessionmanager.close()
 
 
 app = FastAPI(**app_configs, lifespan=lifespan)
@@ -24,6 +29,8 @@ app.add_middleware(
     allow_methods=("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"),
     allow_headers=settings.CORS_HEADERS,
 )
+
+app.include_router(api_router, prefix='/my-service/api/v1')
 
 
 @app.get("/healthcheck", include_in_schema=False)
