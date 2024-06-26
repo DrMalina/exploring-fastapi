0a76a5dca6dc128b0174202afae9223152f540db
Maciej Malinowski <maciej.malinowski7@gmail.com>
2024-06-26 12:20:00 +0200

    feat: add TodoCategoriesService with create and get all methods

---
+++ src/exceptions.py
@@ -0,0 +1,8 @@
+class BaseAppError(Exception):
+    pass
+
+
+class CouldNotReturnCreatedDBRecordError(BaseAppError):
+    def __init__(self, record_name: str) -> None:
+        super().__init__("Could not return record: "
+                         f"'{record_name}' upon writing to the database.")
