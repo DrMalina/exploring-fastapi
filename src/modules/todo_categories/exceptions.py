6106f1e8bff6f2cccff7f7d538b061ab3ffa2f1a
Maciej Malinowski <maciej.malinowski7@gmail.com>
2024-06-26 12:23:44 +0200

    feat: add TodoCategoriesRouter with post and get all routes

---
+++ src/modules/todo_categories/exceptions.py
@@ -0,0 +1,8 @@
+from fastapi import HTTPException
+from starlette import status
+
+
+class TodoCategoryExistsException(HTTPException):
+    def __init__(self, name: str) -> None:
+        super().__init__(status_code=status.HTTP_409_CONFLICT,
+                         detail=f"TodoCategory with a name '{name}' already exists.")
