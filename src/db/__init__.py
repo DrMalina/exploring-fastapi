1490f25a233ee1b56bbb536b9fdd712c85053160
Maciej Malinowski <maciej.malinowski7@gmail.com>
2024-06-26 12:33:09 +0200

    chore: make db a python pkg

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -4,4 +4,5 @@ imported by Alembic. Also, we account for
 SQLAlchemy mapper error when seeding the database.
 See more at https://stackoverflow.com/a/46349722 and https://stackoverflow.com/a/71751680
 """
+
 import src.modules.todo_categories.models  # noqa: F401
