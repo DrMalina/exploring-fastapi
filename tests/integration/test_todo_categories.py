d78e7fee3b1d9fcdad897fe6b945584099bacad4
Maciej Malinowski <maciej.malinowski7@gmail.com>
2024-07-02 12:32:08 +0200

    test: add integration tests for create and get all todo-categories

---
+++ tests/integration/test_todo_categories.py
@@ -0,0 +1,78 @@
+import pytest
+from httpx import AsyncClient
+
+pytestmark = pytest.mark.anyio
+
+
+async def test_create_todo_category__is_unique__creates_and_returns_record(
+    test_client_db: AsyncClient,
+) -> None:
+    response = await test_client_db.post("/api/todo-categories/", json={"name": "Work"})
+    assert response.status_code == 201
+    assert response.json() == {"id": 1, "name": "Work"}
+
+
+async def test_create_todo_category__already_exists__raises_validation_exception(
+    test_client_db: AsyncClient,
+) -> None:
+    response = await test_client_db.post("/api/todo-categories/", json={"name": "Work"})
+    assert response.status_code == 201
+
+    response = await test_client_db.post("/api/todo-categories/", json={"name": "Work"})
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "input": "Work",
+                "loc": ["body", "name"],
+                "msg": "A Todo Category with that name already exists.",
+                "type": "unique",
+            }
+        ]
+    }
+
+
+async def test_create_todo_category__too_long_name__raises_validation_exception(
+    test_client_db: AsyncClient,
+) -> None:
+    too_long_name = "W" * 51
+    response = await test_client_db.post(
+        "/api/todo-categories/", json={"name": too_long_name}
+    )
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "ctx": {"max_length": 50},
+                "input": "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+                "loc": ["body", "name"],
+                "msg": "String should have at most 50 characters",
+                "type": "string_too_long",
+            }
+        ]
+    }
+
+
+async def test_get_all_todo_categories__no_records__returns_empty_list(
+    test_client_db: AsyncClient,
+) -> None:
+    response = await test_client_db.get("/api/todo-categories/")
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+async def test_get_all_todo_categories__existing_records__returns_list_of_categories(
+    test_client_db: AsyncClient,
+) -> None:
+    work_category_response = await test_client_db.post(
+        "/api/todo-categories/", json={"name": "Work"}
+    )
+    assert work_category_response.status_code == 201
+    home_category_response = await test_client_db.post(
+        "/api/todo-categories/", json={"name": "Home"}
+    )
+    assert home_category_response.status_code == 201
+
+    response = await test_client_db.get("/api/todo-categories/")
+    assert response.status_code == 200
+    assert response.json() == [{"id": 1, "name": "Work"}, {"id": 2, "name": "Home"}]
