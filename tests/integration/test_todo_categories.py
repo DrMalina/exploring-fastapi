9835aee67d6a326e118f600f3df8e6e8c24c1343
Maciej Malinowski <maciej.malinowski7@gmail.com>
2024-07-02 18:38:08 +0200

    test: add tests for get single todo-category
    - not exist, returns 404
    - exists, returns record

--- tests/integration/test_todo_categories.py
+++ tests/integration/test_todo_categories.py
@@ -76,3 +76,32 @@ async def test_get_all__existing_records__returns_list_of_categories(
     response = await test_client_db.get("/api/todo-categories/")
     assert response.status_code == 200
     assert response.json() == [{"id": 1, "name": "Work"}, {"id": 2, "name": "Home"}]
+
+
+async def test_get__does_not_exist__returns_404(
+    test_client_db: AsyncClient,
+) -> None:
+    non_existing_id = 99
+    response = await test_client_db.get(f"/api/todo-categories/{non_existing_id}")
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": [
+            {
+                "input": "99",
+                "loc": ["path", "id"],
+                "msg": "TodCategory not found.",
+                "type": "not_found",
+            }
+        ]
+    }
+
+
+async def test_get__exists__returns_record(
+    test_client_db: AsyncClient,
+) -> None:
+    response = await test_client_db.post("/api/todo-categories/", json={"name": "Work"})
+    assert response.status_code == 201
+
+    response = await test_client_db.get("/api/todo-categories/1")
+    assert response.status_code == 200
+    assert response.json() == {"id": 1, "name": "Work"}
