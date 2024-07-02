4f33ffabafcd78a2f4e9f650f6332bbdfa847f7c
Maciej Malinowski <maciej.malinowski7@gmail.com>
2024-07-02 12:01:00 +0200

    feat: add CustomValidationError to raise HTTPExceptions in a pydantic style

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,4 +1,8 @@
+from typing import Any, LiteralString
+
 from fastapi import HTTPException
+from pydantic import ValidationError
+from pydantic_core import InitErrorDetails, PydanticCustomError
 from starlette import status
 
 
@@ -21,4 +25,28 @@ class CustomHTTPException(HTTPException):
         super().__init__(
             status_code=status_code or self.STATUS_CODE,
             detail=[{"msg": msg or self.DETAIL_MSG}],
+        )
+
+
+class CustomValidationError(BaseAppError):
+    def __init__(  # noqa: PLR0913
+        self,
+        title: str,
+        error_type: LiteralString,
+        msg: LiteralString,
+        loc: tuple[int | str, ...],
+        input_value: Any,
+    ) -> None:
+        raise ValidationError.from_exception_data(
+            title=title,
+            line_errors=[
+                InitErrorDetails(
+                    type=PydanticCustomError(
+                        error_type,
+                        msg,
+                    ),
+                    loc=loc,
+                    input=input_value,
+                )
+            ],
         )
