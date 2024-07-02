fe5a6cee8ebe01c784e21ca7614ea6c47ae2dde6
Maciej Malinowski <maciej.malinowski7@gmail.com>
2024-07-02 21:25:59 +0200

    feat: add get route for TodoCategory

--- src/modules/todo_categories/router.py
+++ src/modules/todo_categories/router.py
@@ -3,8 +3,11 @@ from fastapi_restful.cbv import cbv
 from sqlalchemy.exc import IntegrityError
 from starlette import status
 
-from src.exceptions import CustomValidationError
+from src.exceptions import (
+    CustomValidationError,
+)
 from src.modules.todo_categories.dependencies import set_up_todo_categories_service
+from src.modules.todo_categories.exceptions import TodoCategoryNotFound
 from src.modules.todo_categories.schemas import TodoCategory, TodoCategoryCreate
 from src.modules.todo_categories.service import TodoCategoriesService
 from src.utils.db import is_unique_validation_integrity_error
@@ -42,3 +45,13 @@ class TodoCategoriesRouter:
             TodoCategory.model_validate(model)
             for model in await self._service.get_all()
         ]
+
+    @router.get(
+        "/{todo_category_id}",
+        status_code=status.HTTP_200_OK,
+        summary="Get Todo Category",
+    )
+    async def get(self, todo_category_id: int) -> TodoCategory:
+        if not (result := await self._service.get(todo_category_id)):
+            raise TodoCategoryNotFound(todo_category_id, loc=("path", "id"))
+        return TodoCategory.model_validate(result)
