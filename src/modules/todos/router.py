25369efb339e350074bcc1bfd1f17d054e189bc6
Maciej Malinowski <maciej.malinowski7@gmail.com>
2024-07-03 22:57:31 +0200

    feat: add TodosRouter

---
+++ src/modules/todos/router.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends
+from fastapi_restful.cbv import cbv
+from sqlalchemy.exc import IntegrityError
+from starlette import status
+
+from src.modules.todo_categories.exceptions import TodoCategoryNotFound
+from src.modules.todos.dependencies import provide_todo_service
+from src.modules.todos.schemas import Todo, TodoCreate
+from src.modules.todos.service import TodoService
+from src.utils.db import is_foreign_key_validation_integrity_error
+
+router = APIRouter()
+
+
+@cbv(router)
+class TodosRouter:
+    _service: TodoService = Depends(provide_todo_service)
+
+    @router.post("/", status_code=status.HTTP_201_CREATED, summary="Create a Todo")
+    async def create(self, todo: TodoCreate) -> Todo:
+        try:
+            result = await self._service.create(todo)
+            return Todo.model_validate(result)
+        except IntegrityError as ex:
+            if is_foreign_key_validation_integrity_error(ex):
+                raise TodoCategoryNotFound(
+                    todo.todo_category_id, loc=("body", "todo_category_id")
+                ) from ex
+            raise
