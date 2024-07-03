db533b9713623253431cd0d5f89f5a0c8af40676
Maciej Malinowski <maciej.malinowski7@gmail.com>
2024-07-03 22:58:29 +0200

    feat: import todos models into base __init__.py

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -5,4 +5,6 @@ SQLAlchemy mapper error when seeding the database.
 See more at https://stackoverflow.com/a/46349722 and https://stackoverflow.com/a/71751680
 """
 
-import src.modules.todo_categories.models  # noqa: F401
+# ruff: noqa: F401
+import src.modules.todo_categories.models
+import src.modules.todos.models
