cc6931a238a48e3e33d574af6604644afdaac731
Maciej Malinowski <maciej.malinowski7@gmail.com>
2024-07-03 16:43:24 +0200

    refactor: create `valid_todo_category_id` dependency and rename `set_up_todo_categories_service` to `provide_todo_categories_service`

--- src/modules/todo_categories/dependencies.py
+++ src/modules/todo_categories/dependencies.py
@@ -1,8 +1,23 @@
+from typing import Annotated
+
+from fastapi import Depends
+
 from src.dependencies import DBSessionDep
+from src.modules.todo_categories.exceptions import TodoCategoryNotFound
+from src.modules.todo_categories.models import TodoCategory
 from src.modules.todo_categories.service import TodoCategoriesService
 
 
-async def set_up_todo_categories_service(
+async def provide_todo_categories_service(
     db_session: DBSessionDep,
 ) -> TodoCategoriesService:
     return TodoCategoriesService(db_session)
+
+
+async def valid_todo_category_id(
+    todo_category_id: int,
+    service: Annotated[TodoCategoriesService, Depends(provide_todo_categories_service)],
+) -> TodoCategory:
+    if not (result := await service.get(todo_category_id)):
+        raise TodoCategoryNotFound(todo_category_id, loc=("path", "id"))
+    return result
